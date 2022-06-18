96c38ee8ae034765c732dac891d5455897d04131
Saro Amirkhanyan <saroamirkhanyan@gmail.com>
2022-06-18 12:45:02 -0700

    First stable release

---
+++ README.md
@@ -0,0 +1,19 @@
+# MVCS - Metax based Version Control System
+
+### Installation
+```
+	git clone https://github.com/saroamirkhanyan/mvcs
+	cd mvcs 
+	npm i
+	cd ..
+```
+
+### Usage
+
+```
+	mkdir testing
+	node MVCS_DIR/mvcs.js create
+	cd testing
+	echo Hello World > testing
+	node MVCS_DIR/mvcs.js save
+```
--- mvcs.js
+++ mvcs.js
@@ -1,115 +1,44 @@
 'use strict';
-const fsp = require('fs/promises');
+
 const fetch = require('node-fetch');
-const fsUtils = require('./lib/fsUtils.js');
 const MetaxAPI = require('./lib/metax.js');
 const Store = require('./lib/store.js');
-const MVCS = require('./lib/mvcs.js');
+const NodeMVCS = require('./lib/nodemvcs.js');
 
-const metaxAPI = new MetaxAPI({ fetch });
+const metaxAPI = new MetaxAPI({
+  protocol: 'https',
+  hostname: 'ehayq.am',
+  port: 444,
+  fetch,
+});
+
 const store = new Store(metaxAPI);
 
-class NodeMVCS extends MVCS {
-  constructor({ store, cwd }) {
-    super({ store });
-    this.store = store;
-    this.cwd = cwd;
-    this.repoId = null;
-    this.loadRepoId();
-  }
-
-  async loadRepoId() {
-    const repoExists = await fsUtils.fspExists('.mvcs');
-    if (repoExists) {
-      this.repoId = await fsp.readFile('.mvcs');
-    }
-  }
-
-  requireRepo() {
-    if (!this.repoId) {
-      console.error('Repository not found!');
-      return false;
-    }
-    return true;
-  }
-
-  async init() {
-    if (this.repoId) {
-      console.error('Repository already exists');
-      return;
-    }
-    const repoId = await super.init();
-    console.log(repoId);
-  }
-}
-
-const nodemvcs = new NodeMVCS({
-  store,
-  cwd: process.cwd(),
-});
-
-const commands = {
-  init() {
-    nodemvcs.init();
-  },
-};
-
-commands[process.argv[2]]();
-
-/*;(() => {
-	const isRepoInited = await fspExists('.mvcs');
-	let repoId;
-	if(isRepoInited) {
-		repoId = fs.readFile(".mvcs");
-	}
-})();*/
-
-/*;(() =>{
-	const isRepoInited = await fspExists('.mvcs');
-
-	let repoId;
-
-	if(isRepoInited) {
-		repoId = fs.readFile(".mvcs");
-	}
-});
-
-const isRepoInited = await fspExists('.mvcs');
-
-let repoId;
-
-if(isRepoInited) {
-	repoId = fs.readFile(".mvcs");
-}
-
-function checkRepo() {
-	if(isRepoInited) {
-		console.error("Repository is not found");
-		return false;
-	}
-	return true;
-}
-
-const commands = {
-	async save() {
-		if(checkRepo) return;
-		save(repoId);
-	},
-	checkout() {
-	},
-	async init() {
-		const dir = await fsp.readdir('.');
-		if(dir.includes('.mvcs')) {
-			throw new Error("Repository already exsits");
+;(async () => {
+  const nodemvcs = await new NodeMVCS({
+    store,
+    cwd: process.cwd(),
+  });
+	// :TODO Add error handling
+	// :TODO Add usage command
+  const commands = {
+    create() {
+      nodemvcs.create();
+    },
+    save() {
+      const commitId = nodemvcs.save();
+    },
+    async repojson() {
+      const depth = Infinity;
+      const result = await store.load(nodemvcs.repoId, { depth });
+      console.dir(result, { depth });
+    },
+		checkout(commitId) {
+			if(!commitId)
+				throw new Error("Commit id expected!")
+			nodemvcs.checkout(commitId);
 		}
-		repoId = init();
-		fsp.writeFile(".mvcs", repoId);
-	},
-	async repojson() {
-		const depth = 5;
-		const result = await store.load(repoId, { depth });
-		console.dir(result, { depth });
-	}
-}
-
-commands[process.argv[2]]();*/
+  };
+	const args = process.argv;
+  commands[args[2]](args[3]);
+})();
