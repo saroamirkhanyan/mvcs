dc9adb8a7e5b3fcb72d29a0e10a75a432028921e
Saro Amirkhanyan <saroamirkhanyan@gmail.com>
2022-06-19 02:41:56 -0700

    Changes related to npx execution

---
+++ bin/mvcs.js
@@ -0,0 +1,42 @@
+const fetch = require('fetch');
+
+const { MetaxAPI, Store } = require('../mvcs.js');
+
+const metaxAPI = new MetaxAPI({
+  fetch,
+});
+
+const store = new Store(metaxAPI);
+
+(async () => {
+  const nodemvcs = await new NodeMVCS({
+    store,
+    cwd: process.cwd(),
+  });
+  // :TODO Add error handling
+  // :TODO Add usage command
+  const commands = {
+    create() {
+      nodemvcs.create();
+    },
+    save() {
+      const commitId = nodemvcs.save();
+      if (commitId) {
+        console.log('COMMIT', commitId);
+      } else {
+        console.error('No changes to commit');
+      }
+    },
+    async repojson() {
+      const depth = Infinity;
+      const result = await store.load(nodemvcs.repoId, { depth });
+      console.dir(result, { depth });
+    },
+    checkout(commitId) {
+      if (!commitId) throw new Error('Commit id expected!');
+      nodemvcs.checkout(commitId);
+    },
+  };
+  const args = process.argv;
+  commands[args[2]](args[3]);
+})();
--- mvcs.js
+++ mvcs.js
@@ -1,45 +1,13 @@
 'use strict';
 
-const fetch = require('node-fetch');
 const MetaxAPI = require('./lib/metax.js');
 const Store = require('./lib/store.js');
+const MVCS = require('./lib/mvcs.js');
 const NodeMVCS = require('./lib/nodemvcs.js');
 
-const metaxAPI = new MetaxAPI({
-  fetch,
-});
-
-const store = new Store(metaxAPI);
-
-(async () => {
-  const nodemvcs = await new NodeMVCS({
-    store,
-    cwd: process.cwd(),
-  });
-  // :TODO Add error handling
-  // :TODO Add usage command
-  const commands = {
-    create() {
-      nodemvcs.create();
-    },
-    save() {
-      const commitId = nodemvcs.save();
-      if (commitId) {
-        console.log('COMMIT', commitId);
-      } else {
-        console.error('No changes to commit');
-      }
-    },
-    async repojson() {
-      const depth = Infinity;
-      const result = await store.load(nodemvcs.repoId, { depth });
-      console.dir(result, { depth });
-    },
-    checkout(commitId) {
-      if (!commitId) throw new Error('Commit id expected!');
-      nodemvcs.checkout(commitId);
-    },
-  };
-  const args = process.argv;
-  commands[args[2]](args[3]);
-})();
+module.exports = {
+  MetaxAPI,
+  Store,
+  MVCS,
+  NodeMVCS,
+};
